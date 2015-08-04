915f6a1623dd54b28339833ff0b664f90923c4ac
Richard Peter <richard.peter@raytheon.com>
2015-08-04 10:54:58 -0500

    Omaha #4500 - Fix SQL Injection Concerns.
    Change-Id: I6cfd0099b32cbd9c2ed0365bc7a1676cceddf310

--- edex/gov.noaa.nws.ncep.edex.plugin.aww/src/gov/noaa/nws/ncep/edex/plugin/aww/dao/AwwVtecDao.java
+++ edex/gov.noaa.nws.ncep.edex.plugin.aww/src/gov/noaa/nws/ncep/edex/plugin/aww/dao/AwwVtecDao.java
@@ -2,23 +2,35 @@ package gov.noaa.nws.ncep.edex.plugin.aww.dao;
 
 import gov.noaa.nws.ncep.common.dataplugin.aww.AwwVtec;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
-import org.springframework.orm.hibernate4.SessionFactoryUtils;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
 
 import com.raytheon.uf.common.dataquery.db.QueryResult;
-import com.raytheon.uf.common.dataquery.db.QueryResultRow;
 import com.raytheon.uf.edex.database.DataAccessLayerException;
 import com.raytheon.uf.edex.database.dao.CoreDao;
 import com.raytheon.uf.edex.database.dao.DaoConfig;
 
+/**
+ * Dao for AwwVtec.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * ????         ????       ????        Initial creation.
+ * Jun 19, 2015 4500       rjpeter     Removed SQL Injection Concern.
+ * 
+ * </pre>
+ * 
+ * @author rjpeter
+ * @version 1.0
+ */
 public class AwwVtecDao extends CoreDao {
 
     public AwwVtecDao() {
@@ -32,23 +44,23 @@ public class AwwVtecDao extends CoreDao {
         super(config);
     }
 
-
     /**
-     * Returns QueryResult object containing AwwVtec iformation in the database. The
-     * detail information depends on individual SQL query
+     * Returns QueryResult object containing AwwVtec iformation in the database.
+     * The detail information depends on individual SQL query
      * 
      * @return the list of subscriptions
      */
-    public synchronized QueryResult getQueryResultByNativeSQLQuery(String nativeSQLQuery) {
-    	QueryResult queryResult = null; 
-    	try {
-    		queryResult = executeSelectNativeSqlQuery(nativeSQLQuery); 
-    	} catch(ClassCastException cce) {
-    		//do nothing now, if a logger is configured, we will add log message here
-    		//If the ClassCastException is thrown, it means there are no results returned
-    	} catch(DataAccessLayerException dalc) {
-    		//do nothing now, if a logger is configured, we will add log message here
-    	}
+    public synchronized QueryResult getQueryResultByNativeSQLQuery(
+            String nativeSQLQuery) {
+        QueryResult queryResult = null;
+        try {
+            queryResult = executeSelectNativeSqlQuery(nativeSQLQuery);
+        } catch (ClassCastException cce) {
+            // do nothing now, if a logger is configured, we will add log message here
+            // If the ClassCastException is thrown, it means there are no results returned
+        } catch (DataAccessLayerException dalc) {
+            // do nothing now, if a logger is configured, we will add log message here
+        }
         return queryResult;
     }
 
@@ -58,93 +70,45 @@ public class AwwVtecDao extends CoreDao {
      * 
      * @param sql
      *            The sql string
-     * @return A QueryResultObject 
+     * @return A QueryResult Object
      * @throws DataAccessLayerException
      *             If the statement fails
      */
     public QueryResult executeSelectNativeSqlQuery(String selectSQLQuery)
             throws DataAccessLayerException {
-        Session session = null;
-        Connection conn = null;
-        Statement stmt = null;
-        SQLException exception = null;
-        QueryResult results = null;
-
-        try {
-            session = getSession(true);
-            conn = SessionFactoryUtils.getDataSource(getSessionFactory()).getConnection();
-            stmt = conn.createStatement();
-
-        } catch (SQLException e) {
-            throw new DataAccessLayerException(
-                    "Method: executeSelectNativeSqlQuery in AwwVtecDao class, Unable to create JDBC statement", e);
-        }
-        try {
-            ResultSet resultSet = stmt.executeQuery(selectSQLQuery); 
-            results = mapResultSet(resultSet);
-
-        } catch (SQLException e1) {
-            exception = e1;
-            logger.error("Error executing script="+selectSQLQuery+",  ", e1);
-        }
-
-        try {
-            stmt.close();
-        } catch (SQLException e1) {
-            exception = e1;
-            logger.error("Method: executeSelectNativeSqlQuery in AwwVtecDao class, Unable to close JDBC statement!", e1);
-        }
-
-        try {
-            if (!conn.isClosed()) {
-                conn.close();
-            }
-        } catch (SQLException e) {
-            exception = e;
-            logger.error("Method: executeSelectNativeSqlQuery in AwwVtecDao class, Cannot close database connection!!", e);
-        }
-        if (session.isOpen()) {
-            session.close();
-        }
-        if (exception != null) {
-            throw new DataAccessLayerException(
-                    "Method: executeSelectNativeSqlQuery in AwwVtecDao class, SQLException is wrapped and rethrown in DataAccessLayerException", exception);
-        }
-        return results;
+        return executeMappedSQLQuery(selectSQLQuery);
     }
 
-    /**
-     * Helper method for mapping JDBC result sets
-     * this method is private in coreDao, it is copied here
-     * 
-     * @param rs
-     *            The raw ResultSet object
-     * @return The remapped results
-     * @throws SQLException
-     *             If mapping fails
-     */
-    private QueryResult mapResultSet(ResultSet rs) throws SQLException {
-        QueryResult results = new QueryResult();
+    public void populateAwwVtecEventStartTimeWithValidValue(
+            final Calendar validEventStartTime, final String productClass,
+            final String officeId, final String phenomena,
+            final String significance, final String eventTrackingNumber)
+            throws DataAccessLayerException {
+        txTemplate.execute(new TransactionCallback<Integer>() {
+            @Override
+            public Integer doInTransaction(TransactionStatus status) {
+                StringBuilder queryString = new StringBuilder(160);
+                queryString
+                        .append("update AwwVtec set eventStartTime = :eventStartTime where");
+                queryString.append(" eventStartTime is null");
+                queryString
+                        .append(" and eventTrackingNumber = :eventTrackingNumber");
+                queryString.append(" and officeID = :officeID");
+                queryString.append(" and phenomena = :phenomena");
+                queryString.append(" and productClass = :productClass");
+                queryString.append(" and significance = :significance");
 
-        ResultSetMetaData metadata = rs.getMetaData();
+                Session sess = getCurrentSession();
+                Query query = sess.createQuery(queryString.toString());
+                query.setCalendar("eventStartTime", validEventStartTime);
+                query.setString("eventTrackingNumber", eventTrackingNumber);
+                query.setString("officeID", officeId);
+                query.setString("phenomena", phenomena);
+                query.setString("productClass", productClass);
+                query.setString("significance", significance);
 
-        int columnCount = metadata.getColumnCount();
-        for (int i = 0; i < columnCount; i++) {
-            results.addColumnName(metadata.getColumnLabel(i + 1), i);
-        }
-
-        List<QueryResultRow> rows = new ArrayList<QueryResultRow>();
-        while (rs.next()) {
-            Object[] columnValues = new Object[columnCount];
-            for (int i = 1; i <= columnCount; i++) {
-                columnValues[i - 1] = rs.getObject(i);
-
+                return query.executeUpdate();
             }
-            rows.add(new QueryResultRow(columnValues));
-        }
-        results.setRows(rows.toArray(new QueryResultRow[] {}));
-        return results;
+        });
     }
-
-
 }

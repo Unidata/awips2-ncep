3066dafbd7396cb74d30790c82ebfa01beb022a9
mjames-upc <mjames@ucar.edu>
2016-01-18 16:55:40 -0600

    NC sat resource colormaps fix to check for cmap in both ncep and common_static

--- viz/gov.noaa.nws.ncep.viz.common/src/gov/noaa/nws/ncep/viz/common/ColorMapUtil.java
+++ viz/gov.noaa.nws.ncep.viz.common/src/gov/noaa/nws/ncep/viz/common/ColorMapUtil.java
@@ -18,6 +18,7 @@ import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationContext;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationLevel;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationType;
+import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationFile;
 import com.raytheon.uf.common.localization.PathManagerFactory;
 import com.raytheon.uf.common.localization.SaveableOutputStream;
@@ -47,6 +48,7 @@ import com.raytheon.uf.viz.core.exception.VizException;
  * 04/10/2013   #958         qzhou       Added SolarImage in getColorMapCategories.
  * 08/06/2013   2210         njensen     Moved colormaps to common_static
  * Nov 11, 2013 2361         njensen     Use ColorMap.JAXB for XML processing
+ * 01/18/2016   ----         mjames@ucar Check common_static if ncpath is null
  * 05/17/2016   R18398       S. Russell  Updated method loadLockedColorMap()
  *                                       and removed deprecated method calls to
  *                                       deprecated LocalizationFile.getFile(),
@@ -64,6 +66,10 @@ public class ColorMapUtil {
 
     ColorMapUtil() {
     }
+    
+    public static final String EXTENSION = ".cmap";
+    
+    public static final String DIR_NAME = "colormaps";
 
     /**
      * Load a colormap by name
@@ -91,22 +97,26 @@ public class ColorMapUtil {
                     + File.separator + name + ".cmap";
 
             try {
-
-                File f = PathManagerFactory.getPathManager().getStaticFile(
-                        fname);
+            	// Check localization/ncep/ColorMaps first
+            	// Check common_static/base/colormaps second
+            	String ncPath = NcPathConstants.COLORMAPS_DIR + File.separator
+                        + cmapCat + File.separator + name + ".cmap";
+                File f = NcPathManager.getInstance().getStaticFile(ncPath);
 
                 if (f != null) {
-
-                    // JAXBManager.unmarshalFromInputStream(InputStream) does
-                    // not work here. openInputStream() also depends on the
-                    // deprecated getFile().
-                    cm = ColorMap.JAXB
-                            .unmarshalFromXmlFile(f.getAbsolutePath());
-
+                    cm = ColorMap.JAXB.unmarshalFromXmlFile(f
+                            .getAbsolutePath());
                     cm.setName(name);
 
                 } else {
-                    throw new VizException("Error finding colormap " + fname);
+                	 LocalizationFile lf = PathManagerFactory.getPathManager()
+                            .getStaticLocalizationFile(
+                                    DIR_NAME + IPathManager.SEPARATOR + name
+                                            + EXTENSION);
+                	File lfFile = lf.getFile();
+                	cm = ColorMap.JAXB.unmarshalFromXmlFile(lfFile);
+					cm.setName(name);
+					return cm;
                 }
             } catch (Exception e) {
                 throw new VizException("Error umarshalling colormap " + fname,
--- viz/gov.noaa.nws.ncep.viz.rsc.satellite/src/gov/noaa/nws/ncep/viz/rsc/satellite/rsc/NcSatelliteResource.java
+++ viz/gov.noaa.nws.ncep.viz.rsc.satellite/src/gov/noaa/nws/ncep/viz/rsc/satellite/rsc/NcSatelliteResource.java
@@ -113,6 +113,7 @@ import gov.noaa.nws.ncep.viz.ui.display.NCMapDescriptor;
  *  ------------ ---------- ----------- --------------------------
  *  09/28/2015   R11385     njensen     Initial creation
  *  10/28/2015   R11385     kbugenhagen Updated to use stylerules to generate colormap
+ *  01/18/2016   ------     mjames@ucar Check common_static if ncpath is null
  *  04/12/2016   R16367     kbugenhagen Added support for SIMGOESR
  *  04/15/2016   R15954     SRussell    Updated FrameData.updateFrameData()
  *                                      Integrated class with IDataLoader design
@@ -673,6 +674,7 @@ public class NcSatelliteResource extends
                 List<StyleRule> styleRuleList = styleSet.getStyleRules();
                 for (StyleRule sr : styleRuleList) {
                     MatchCriteria styleMatchCriteria = sr.getMatchCriteria();
+                    Integer matchValue = styleMatchCriteria.matches(matchCriteria);
                     if (styleMatchCriteria.matches(matchCriteria) > 0) {
                         styleRule = sr;
                         break;
@@ -1060,6 +1062,19 @@ public class NcSatelliteResource extends
             imgCap.setContrast(resourceData.getContrast());
             imgCap.setAlpha(resourceData.getAlpha());
             paintProps.setAlpha(resourceData.getAlpha());
+
+            ColorMapParameters params = getCapability(ColorMapCapability.class)
+                    .getColorMapParameters();
+                String colorMapName = resourceData.getColorMapName();
+                if (colorMapName == null) {
+                    colorMapName = "Sat/VIS/ZA (Vis Default)";
+                }
+                
+                // the D2D name is sent here, which was unaccounted for in ColorMapUtil
+                params.setColorMap(ColorMapUtil.loadColorMap(
+                        ResourceCategory.SatelliteRscCategory.getCategoryName(),
+                        colorMapName));
+
             satr.paint(target, paintProps);
         }
     }

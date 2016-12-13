358aaf07ee741c293d7972462b70f8a7591b0cb5
Jeffrey Beck <Jeffrey.Beck@noaa.gov>
2016-12-13 16:48:01 -0500

    VLab Issue #25982 - EDEX>NCTEXT - Not all TAFs being decoded
    Change-Id: I8ec58910d1ab299570ea62d9acd6481fd0c2b1d5

--- viz/gov.noaa.nws.ncep.ui.nctextui/src/gov/noaa/nws/ncep/ui/nctextui/dbutil/NctextStationInfo.java
+++ viz/gov.noaa.nws.ncep.ui.nctextui/src/gov/noaa/nws/ncep/ui/nctextui/dbutil/NctextStationInfo.java
@@ -1,65 +1,107 @@
+/**
+* 
+* gov.noaa.nws.ncep.ui.nctextui.dbutil.NctextStationInfo
+* This class provides represents an nctext station
+* 
+* <pre>
+* 
+* SOFTWARE HISTORY
+* 
+* Date         Ticket#     Engineer    Description
+* -------      -------     --------    -----------
+* 12/12/2016   R25982      Jeff Beck   Add implements Comparable to enable Collections.sort()
+*                                      Add compareTo()                                  
+* </pre>
+* 
+* @author Chin Chen
+* @version 1.0
+*/
 package gov.noaa.nws.ncep.ui.nctextui.dbutil;
 
-import java.util.List;
+public class NctextStationInfo implements Comparable<NctextStationInfo> {
 
-public class NctextStationInfo {
-	protected String productid; //WMO id
-	protected String stnid;
-	protected String stnname;
-	protected String state;
-	protected String country;
-	protected double latitude;
-	protected double longitude;
-	protected Integer elevation;
-	
-	
-	public String getProductid() {
-		return productid;
-	}
-	public void setProductid(String productid) {
-		this.productid = productid;
-	}
-	public String getStnid() {
-		return stnid;
-	}
-	public void setStnid(String stnid) {
-		this.stnid = stnid;
-	}
-	public String getStnname() {
-		return stnname;
-	}
-	public void setStnname(String stnname) {
-		this.stnname = stnname;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public double getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-	public double getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
-	public Integer getElevation() {
-		return elevation;
-	}
-	public void setElevation(Integer elevation) {
-		this.elevation = elevation;
-	}
+    protected String productid; // WMO id
+
+    protected String stnid;
+
+    protected String stnname;
+
+    protected String state;
+
+    protected String country;
+
+    protected double latitude;
+
+    protected double longitude;
+
+    protected Integer elevation;
+
+    public String getProductid() {
+        return productid;
+    }
+
+    public void setProductid(String productid) {
+        this.productid = productid;
+    }
+
+    public String getStnid() {
+        return stnid;
+    }
+
+    public void setStnid(String stnid) {
+        this.stnid = stnid;
+    }
+
+    public String getStnname() {
+        return stnname;
+    }
+
+    public void setStnname(String stnname) {
+        this.stnname = stnname;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(Integer elevation) {
+        this.elevation = elevation;
+    }
+
+    public int compareTo(NctextStationInfo other) {
+        return this.getStnid().compareTo(other.getStnid());
+    }
 
 }

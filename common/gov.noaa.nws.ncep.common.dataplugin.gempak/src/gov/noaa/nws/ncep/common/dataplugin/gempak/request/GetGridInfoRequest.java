65ac0ed86c2f2aa37025fd9dfa1073ce56b4968e
mjames-upc <mjames@unidata.ucar.edu>
2017-03-31 14:12:53 -0600

    Revert "update GridInfoRequest from ncep 16.4.1"
    This reverts commit d912f6b38a1fb9d6e6bafccdc1f26847969df4f1.

--- common/gov.noaa.nws.ncep.common.dataplugin.gempak/src/gov/noaa/nws/ncep/common/dataplugin/gempak/request/GetGridInfoRequest.java
+++ common/gov.noaa.nws.ncep.common.dataplugin.gempak/src/gov/noaa/nws/ncep/common/dataplugin/gempak/request/GetGridInfoRequest.java
@@ -4,25 +4,6 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 import com.raytheon.uf.common.serialization.comm.IServerRequest;
 
-/**
- * 
- * Contains information used to request grid field information from the EDEX
- * data stores. Users can specify model, reference time, and or forecast time.
- * 
- * <pre>
- * 
- * SOFTWARE HISTORY
- * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * ??? ??, ????            Jacobs     Initial creation
- * Sep 14, 2015 RM8764     Gilbert    Added reftime and fcstsec fields
- * 
- * </pre>
- * 
- * @author sgilbert
- * @version 1.0
- */
 @DynamicSerialize
 public class GetGridInfoRequest implements IServerRequest {
 
@@ -31,12 +12,6 @@ public class GetGridInfoRequest implements IServerRequest {
 
     @DynamicSerializeElement
     private String modelId;
-
-    @DynamicSerializeElement
-    private String reftime;
-
-    @DynamicSerializeElement
-    private String fcstsec;
 
     public GetGridInfoRequest() {
     }
@@ -55,22 +30,6 @@ public class GetGridInfoRequest implements IServerRequest {
 
     public void setModelId(String modelId) {
         this.modelId = modelId;
-    }
-
-    public String getReftime() {
-        return reftime;
-    }
-
-    public void setReftime(String reftime) {
-        this.reftime = reftime;
-    }
-
-    public String getFcstsec() {
-        return fcstsec;
-    }
-
-    public void setFcstsec(String fcstsec) {
-        this.fcstsec = fcstsec;
     }
 
 }

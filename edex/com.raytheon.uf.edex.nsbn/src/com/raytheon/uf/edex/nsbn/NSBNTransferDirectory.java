7171211005a448c76edfcc51651300d30cd2df92
Timothy Jensen <timothy.jensen@noaa.gov>
2018-12-10 11:29:17 -0600

    VLab Issue #56039 - Refactor NSBN threading and add idempotent registry
    Change-Id: Ic9690a15e6357013461c6c1059ae5cccb2bd1439

--- edex/com.raytheon.uf.edex.nsbn/src/com/raytheon/uf/edex/nsbn/NSBNTransferDirectory.java
+++ edex/com.raytheon.uf.edex.nsbn/src/com/raytheon/uf/edex/nsbn/NSBNTransferDirectory.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -33,24 +32,22 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.raytheon.uf.common.status.IUFStatusHandler;
-import com.raytheon.uf.common.status.UFStatus;
-
 /**
- * A container class to hold which directory configurations
- * apply to a transfer data type.
- * 
+ * A container class to hold which directory configurations apply to a transfer
+ * data type.
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
+ *
  * Date          Ticket#  Engineer    Description
  * ------------- -------- ----------- --------------------------
  * Oct 18, 2018  56039    mrichardson Initial Creation
- * 
- * 
+ * Dec 10, 2018  56039    tjensen     Default threadpool count to 0
+ *
+ *
  * </pre>
- * 
+ *
  * @author mrichardson
  */
 
@@ -62,7 +59,7 @@ public class NSBNTransferDirectory implements FileFilter {
     private String id;
 
     @XmlElement
-    private int threadpoolCount;
+    private int threadpoolCount = 0;
 
     @XmlElement
     private String scanDir;
@@ -73,24 +70,24 @@ public class NSBNTransferDirectory implements FileFilter {
     @XmlElement
     private String destinationDir;
 
-    @XmlElements({ @XmlElement(name="include") })
+    @XmlElements({ @XmlElement(name = "include") })
     private List<String> include;
-    
-    private List<Pattern> includePatterns = new ArrayList<Pattern>();
 
-    @XmlElements({ @XmlElement(name="exclude") })
+    private final List<Pattern> includePatterns = new ArrayList<>();
+
+    @XmlElements({ @XmlElement(name = "exclude") })
     private List<String> exclude;
-    
-    private List<Pattern> excludePatterns = new ArrayList<Pattern>();
+
+    private final List<Pattern> excludePatterns = new ArrayList<>();
 
     public NSBNTransferDirectory() {
 
     }
-    
+
     @Override
     public boolean accept(File f) {
         boolean accept = true;
-        
+
         // Exclude any file that matches against an exclude regex
         if (!excludePatterns.isEmpty()) {
             for (Pattern exc : excludePatterns) {
@@ -100,10 +97,10 @@ public class NSBNTransferDirectory implements FileFilter {
                 }
             }
         }
-        
+
         if (accept) {
             // Include any file that matches against an include regex
-            //  or if an include regex has been declared, but is empty
+            // or if an include regex has been declared, but is empty
             if (!includePatterns.isEmpty()) {
                 for (Pattern inc : includePatterns) {
                     accept = inc.matcher(f.getName()).find();
@@ -113,7 +110,7 @@ public class NSBNTransferDirectory implements FileFilter {
                 }
             }
         }
-        
+
         return accept;
     }
 
@@ -136,11 +133,11 @@ public class NSBNTransferDirectory implements FileFilter {
     public String getScanDir() {
         return scanDir;
     }
-    
+
     public void setScanDir(String scanDir) {
         this.scanDir = scanDir;
     }
-    
+
     public String getDestinationQueue() {
         return destinationQueue;
     }
@@ -148,7 +145,7 @@ public class NSBNTransferDirectory implements FileFilter {
     public void setDestinationQueue(String destinationQueue) {
         this.destinationQueue = destinationQueue;
     }
-    
+
     public String getDestinationDir() {
         return destinationDir;
     }
@@ -172,7 +169,7 @@ public class NSBNTransferDirectory implements FileFilter {
     public void setExclude(List<String> exclude) {
         this.exclude = exclude;
     }
-    
+
     public void compileRegexes() {
         for (String inc : include) {
             if (inc != null && !inc.isEmpty()) {

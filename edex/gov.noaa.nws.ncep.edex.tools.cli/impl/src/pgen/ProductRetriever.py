177c9b7d204915c971fb6b656754a69c2e624b0d
Tom Gurney <tom.gurney@raytheon.com>
2019-06-26 17:03:08 -0500

    Omaha #7883 Python 3 fixes for NCEP CLI tools
    Change-Id: I0f0f37f0e3be018b432497384927c52301e03bdc

--- edex/gov.noaa.nws.ncep.edex.tools.cli/impl/src/pgen/ProductRetriever.py
+++ edex/gov.noaa.nws.ncep.edex.tools.cli/impl/src/pgen/ProductRetriever.py
@@ -9,19 +9,19 @@ class ProductRetriever:
     """ Retrieves all PGEN products for a given Activity (dataURI) and writes them to separate files."""
 
     def __init__(self,dataURI,label):
-         self.dataURI = dataURI
-         self.label = label
-         self.fullpath = False
-         self.outdir = os.getcwd()
-         self.host = os.getenv("DEFAULT_HOST", "localhost")
-         self.port = os.getenv("DEFAULT_PORT", "9581")
-         self.client = ThriftClient.ThriftClient(self.host, self.port)
+        self.dataURI = dataURI
+        self.label = label
+        self.fullpath = False
+        self.outdir = os.getcwd()
+        self.host = os.getenv("DEFAULT_HOST", "localhost")
+        self.port = os.getenv("DEFAULT_PORT", "9581")
+        self.client = ThriftClient.ThriftClient(self.host, self.port)
 
     def setOutputDir(self, outdir):
-         self.outdir = outdir
-    
+        self.outdir = outdir
+
     def setFullpath(self, fullpath):
-         self.fullpath = fullpath
+        self.fullpath = fullpath
 
     def _writeout(self,filename,bytes):
         outname = self.outdir + str(os.sep) + filename
@@ -30,31 +30,30 @@ class ProductRetriever:
         f.close()
 
     def getProducts(self):
-         """ Sends ThriftClient request and writes out received files."""
-         req = RetrieveAllProductsRequest()
-         req.setDataURI(self.dataURI)
-         resp = self.client.sendRequest(req)
-         #os.mkdir(self.outdir)
-         for item in resp:
-             if item.getName() == "ActivityXML":
-                 if self.label.endswith(".xml"):
-                     filename = self.label
-                 else:
-                     filename = self.label + ".xml"
-             else:
-                 filename = item.getName()
-             
-             if ( self.fullpath ):
-                 path = self.dataURI
-                 fname = path.replace("/", ".") + "$" + filename
-                 filename = fname.lstrip().strip(".").replace("..", ".")
-         
-         
-             if isinstance(item, StringDataRecord):
-                 self._writeout(filename,item.getStringData()[0])
-             elif isinstance(item, ByteDataRecord):
-                 self._writeout(filename,item.getByteData())
-             print "Extracted... " + filename
+        """ Sends ThriftClient request and writes out received files."""
+        req = RetrieveAllProductsRequest()
+        req.setDataURI(self.dataURI)
+        resp = self.client.sendRequest(req)
+        #os.mkdir(self.outdir)
+        for item in resp:
+            if item.getName() == "ActivityXML":
+                if self.label.endswith(".xml"):
+                    filename = self.label
+                else:
+                    filename = self.label + ".xml"
+            else:
+                filename = item.getName()
 
-         return resp
-             
+            if ( self.fullpath ):
+                path = self.dataURI
+                fname = path.replace("/", ".") + "$" + filename
+                filename = fname.lstrip().strip(".").replace("..", ".")
+
+
+            if isinstance(item, StringDataRecord):
+                self._writeout(filename,item.getStringData()[0])
+            elif isinstance(item, ByteDataRecord):
+                self._writeout(filename,item.getByteData())
+            print("Extracted... " + filename)
+
+        return resp
